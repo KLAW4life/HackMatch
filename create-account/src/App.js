79871e7fd8eb7bdd4f9f8f6adbb65c50ba37bfda
tatiana <122471667+tatianar3@users.noreply.github.com>
2024-09-28 19:41:56 -0400

    Create App.js

---
+++ create-account/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import InfoForm from './info-form/info-form';
+
+const App = () => {
+    return (
+        <div>
+            <InfoForm/>
+        </div>
+    );
+};
+
+export default App;
