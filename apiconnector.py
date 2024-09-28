4759607efb7132c99d42158fc1bd3a56ba462a9f
KLAW4life <kerene789@gmail.com>
2024-09-28 09:42:36 -0400

    Database connection established

--- apiconnector.py
+++ apiconnector.py
@@ -1,5 +1,4 @@
 import mysql.connector
-# import jsonify
 import json
 
 ''' 
@@ -12,6 +11,7 @@ db_config = {
     'auth_plugin': 'mysql_native_password'  
 }
 
+
 # Database connection configuration for Mac/Windows
 db_config = {
     'host': 'localhost',        
@@ -21,34 +21,57 @@ db_config = {
 }
 '''
 
+#Test User
+db_config = {
+    'host': 'localhost',        
+    'user': 'root',        
+    'password': 'Kl@w$li3e!',
+    'database': 'HackerMatch',
+    'auth_plugin': 'mysql_native_password'
+}
+
 def get_users():
     try:
-
-       #Use test person to connect to the database. Reconfigure per person
-        mydb = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Kl@w$li3e!',
-        database='HackerMatch',
-        auth_plugin='mysql_native_password'
-        )
+        # Connect to the database
+        mydb = mysql.connector.connect(**db_config)
         
         mycursor = mydb.cursor()
         sql = "SELECT * FROM Users"
         mycursor.execute(sql)
         results = mycursor.fetchall()
-        
+
+        # Close the cursor and connection
         mycursor.close()
         mydb.close()
-        return results
+        
+        # Convert the results to a list of dictionaries for easy JSON conversion
+        users = []
+        for row in results:
+            user = {
+                "id": row[0],
+                "firstname": row[1],
+                "lastname": row[2],
+                "email": row[3],
+                "username": row[4]
+            }
+            users.append(user)
+        
+        return json.dumps(users, indent=4)  # Return as a formatted JSON string
     except mysql.connector.Error as e:
         print("Error fetching users:", e)
         return []
-    
-    
+
+
 def main():
-    get_users()
+    users = get_users()
+    if users:
+        print(users)
 
 
 if __name__ == "__main__":
   main()
+
+
+
+
+
